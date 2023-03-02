c5b78d1f2f21aeb1efce82d87f5b3bed2a4b5ce5
Pravith <pravith10@gmail.com>
2023-03-02 13:56:40 +0530

    Features: 1. My changes

--- lua/pravith/remap.lua
+++ lua/pravith/remap.lua
@@ -1,7 +1,9 @@
 
 vim.g.mapleader = " "
+-- Open explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+--
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
