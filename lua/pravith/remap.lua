d85b0f7520358a16dc6e4525ff6dd560a0bf2d06
PravithBA <pravithba10@gmail.com>
2023-05-24 11:13:20 +0530

    Feature: Changed the remap of lsp format to "<leader>ff", because the "<leader>fg" remap was made, that made "<leader>f" slow.

--- lua/pravith/remap.lua
+++ lua/pravith/remap.lua
@@ -49,7 +49,7 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- TBF
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
