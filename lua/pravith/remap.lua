a10b4ef79c2b26ed16427b1a0b9983ee68ce3040
Pravith <pravith@getfinstack.in>
2023-05-26 15:13:39 +0530

    Features: 1. Added a new command which will select everything in any mode hehe. 2. Formatted and deleted some shortcuts which werent usefull.

--- lua/pravith/remap.lua
+++ lua/pravith/remap.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = " "
 -- Open explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -7,7 +6,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- JoiJn Jthe bottom line and this line, i.e puts the bottom line to the end of the current line
+-- Join the bottom line and this line, i.e puts the bottom line to the end of the current line
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- To move up and down, but it puts the cursor in the middle
@@ -18,23 +17,15 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- IDK
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- Paste from clipboard
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Copy to clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- TBF
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- Deletes without copying (WOW)
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Obvious lol
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -47,7 +38,7 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
--- TBF
+-- Formats code using lsp lel
 
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
 
@@ -59,9 +50,12 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/pravith/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/pravith/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- Select all from any mode
+vim.keymap.set({"n", "v", "i"}, "<C-d>", "<C-c>ggVG")
