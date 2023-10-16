54aa8e90ddcf6c4c8f7dca82e7b83f518e816dce
Pravith <pravith@getfinstack.in>
2023-10-16 18:27:37 +0530

    Feature: Removed the branch constraint on telescope.

--- lua/pravith/packer.lua
+++ lua/pravith/packer.lua
@@ -12,7 +12,7 @@ return require('packer').startup(function(use)
 
     -- Telescope, duh
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim',
         -- or                            , branch = '0.1.x',
         requires = {
             { 'nvim-lua/plenary.nvim' },
